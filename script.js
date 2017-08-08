5469213adc74d31b38964066f78719b323bd1c68
Yih Wen <ywchang@smu.edu.sg>
2017-08-08 17:56:50 +0800

    Insert menu items

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function nav(){
+function showNav(){
 	//alert('rgt');
+	//$('nav').toggleClass('togNav');
 	$('nav').toggleClass('togNav');
 	$('main').toggleClass('togMain');
+}
+
+function showResume(){
+	//alert($(this).attr("href"));
+	window.open("myresume/myresume.html", "width=600,height=600,scrollbars=yes");
 }
